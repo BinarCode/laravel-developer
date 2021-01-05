38b16f20602c902394d45eb78577a7171be0415d
binaryk <binaryk@users.noreply.github.com>
2021-01-05 09:28:23 +0000

    Fix styling

--- tests/Fixtures/User.php
+++ tests/Fixtures/User.php
@@ -10,7 +10,7 @@ class User
 
     public function createToken(string $name, array $abilities = ['*'])
     {
-        return tap(new PersonalAccessToken(), function(PersonalAccessToken $token) {
+        return tap(new PersonalAccessToken(), function (PersonalAccessToken $token) {
             $token->plainTextToken = 'foo';
         });
     }
