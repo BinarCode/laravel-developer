26acb9c9125f74c2f3f1725d8d7f23ceada0eca0
Eduard Lupacescu <lupacescueduard@gmail.com>
2021-01-20 11:05:36 +0200

    patch

--- src/LaravelDeveloperServiceProvider.php
+++ src/LaravelDeveloperServiceProvider.php
@@ -51,6 +51,8 @@ class LaravelDeveloperServiceProvider extends ServiceProvider
 
             return $this;
         });
+
+        return $this;
     }
 
     public static function migrationFileExists(string $migrationFileName): bool
